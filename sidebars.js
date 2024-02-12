8209dd5332258e5cfd0d46df9fa48507342ab1ed
0yukali0 <a08h0283@gmail.com>
2024-02-12 19:59:19 +0800

    sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,15 +1,88 @@
 module.exports = {
   docs: {
     'Hadoop Systems': [
-      'hadoop_systems/hadoop',
-      'hadoop_systems/hbase',
-      'hadoop_systems/ozone',
-      'hadoop_systems/spark',
-      'hadoop_systems/zookeeper'
+      {
+        type: 'category',
+        label: 'Hadoop',
+        items: [
+          'hadoop/buildhadoop'
+        ]
+      },
+      {
+        type: 'category',
+        label: 'HBase',
+        items: [
+          'hbase/buildhbase'
+        ]
+      },
+      {
+        type: 'category',
+        label: 'Ozone',
+        items: [
+          'ozone/buildozone'
+        ]
+      },
+      {
+        type: 'category',
+        label: 'Spark',
+        items: [
+          'spark/buildspark'
+        ]
+      },
+      {
+        type: 'category',
+        label: 'Zookeeper',
+        items: [
+          'zookeeper/buildzookeeper'
+        ]
+      }
     ],
     'Machine Learning': [
-      'machinelearning/devtools',
-      'machinelearning/distributedframework'
+      {
+        type: 'category',
+        label: 'pytorch',
+        items: [
+          {
+            type: 'category',
+            lablel: 'kaggle',
+            items: [
+              'pytorch/kaggle/cancer',
+              'pytorch/kaggle/datasciencelondon',
+              'pytorch/kaggle/mnist'
+            ]
+          },
+          {
+            type: 'category',
+            lablel: 'lightning',
+            items: [
+              'pytorch/lightning/mnist',
+            ]
+          },
+        ]
+      },
+      {
+        type: 'category',
+        label: 'ray',
+        items: [
+          {
+            type: 'category',
+            lablel: 'train',
+            items: [
+              'ray/train/train_overview',
+              'ray/train/train_framework',
+              'ray/train/train',
+              {
+                type: 'category',
+                label: 'kaggle',
+                items: [
+                  'ray/train/kaggle/cancer',
+                  'ray/train/kaggle/datasciencelondon'
+                ]
+              }
+            ]
+          }
+        ]
+      }
     ]
   },
 };
